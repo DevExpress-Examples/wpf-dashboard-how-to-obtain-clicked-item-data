f707fa3b75edda1e50d3d57b31916e50a6c90bce
DevExpressExampleBot <fakeEmail>
2018-08-24 16:49:35 +0300

    Source auto update [skip ci]

---
+++ VB/Dashboard_ClientDataCards_Wpf/Dashboard1.Designer.vb
@@ -0,0 +1,167 @@
+﻿Namespace Dashboard_ClientDataCards_Wpf
+    Partial Public Class Dashboard1
+        ''' <summary> 
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Component Designer generated code"
+
+        ''' <summary> 
+        ''' Required method for Designer support - do not modify 
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim dimension1 As New DevExpress.DashboardCommon.Dimension()
+            Dim measure1 As New DevExpress.DashboardCommon.Measure()
+            Dim dimension2 As New DevExpress.DashboardCommon.Dimension()
+            Dim selectQuery1 As New DevExpress.DataAccess.Sql.SelectQuery()
+            Dim allColumns1 As New DevExpress.DataAccess.Sql.AllColumns()
+            Dim table1 As New DevExpress.DataAccess.Sql.Table()
+            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Dashboard1))
+            Dim measure2 As New DevExpress.DashboardCommon.Measure()
+            Dim measure3 As New DevExpress.DashboardCommon.Measure()
+            Dim card1 As New DevExpress.DashboardCommon.Card()
+            Dim cardCompactLayoutTemplate1 As New DevExpress.DashboardCommon.CardCompactLayoutTemplate()
+            Dim dimension3 As New DevExpress.DashboardCommon.Dimension()
+            Dim dimension4 As New DevExpress.DashboardCommon.Dimension()
+            Dim dashboardLayoutGroup1 As New DevExpress.DashboardCommon.DashboardLayoutGroup()
+            Dim dashboardLayoutItem1 As New DevExpress.DashboardCommon.DashboardLayoutItem()
+            Dim dashboardLayoutItem2 As New DevExpress.DashboardCommon.DashboardLayoutItem()
+            Me.pieDashboardItem1 = New DevExpress.DashboardCommon.PieDashboardItem()
+            Me.dashboardSqlDataSource1 = New DevExpress.DashboardCommon.DashboardSqlDataSource()
+            Me.cardDashboardItem1 = New DevExpress.DashboardCommon.CardDashboardItem()
+            CType(Me.pieDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(dimension1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(measure1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(dimension2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.dashboardSqlDataSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.cardDashboardItem1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(measure2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(measure3, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(dimension3, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(dimension4, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' pieDashboardItem1
+            ' 
+            dimension1.DataMember = "OrderDate"
+            Me.pieDashboardItem1.Arguments.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension1})
+            Me.pieDashboardItem1.ComponentName = "pieDashboardItem1"
+            measure1.DataMember = "Extended Price"
+            dimension2.DataMember = "Country"
+            Me.pieDashboardItem1.DataItemRepository.Clear()
+            Me.pieDashboardItem1.DataItemRepository.Add(measure1, "DataItem0")
+            Me.pieDashboardItem1.DataItemRepository.Add(dimension1, "DataItem1")
+            Me.pieDashboardItem1.DataItemRepository.Add(dimension2, "DataItem2")
+            Me.pieDashboardItem1.DataMember = "SalesPerson"
+            Me.pieDashboardItem1.DataSource = Me.dashboardSqlDataSource1
+            Me.pieDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
+            Me.pieDashboardItem1.Name = "Pies 1"
+            Me.pieDashboardItem1.SeriesDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension2})
+            Me.pieDashboardItem1.ShowCaption = True
+            Me.pieDashboardItem1.Values.AddRange(New DevExpress.DashboardCommon.Measure() { measure1})
+            ' 
+            ' dashboardSqlDataSource1
+            ' 
+            Me.dashboardSqlDataSource1.ComponentName = "dashboardSqlDataSource1"
+            Me.dashboardSqlDataSource1.ConnectionName = "nwind.mdb"
+            Me.dashboardSqlDataSource1.Name = "SQL Data Source 1"
+            table1.MetaSerializable = "<Meta X=""30"" Y=""30"" Width=""125"" Height=""286"" />"
+            table1.Name = "SalesPerson"
+            allColumns1.Table = table1
+            selectQuery1.Columns.Add(allColumns1)
+            selectQuery1.Name = "SalesPerson"
+            selectQuery1.Tables.Add(table1)
+            Me.dashboardSqlDataSource1.Queries.AddRange(New DevExpress.DataAccess.Sql.SqlQuery() { selectQuery1})
+            Me.dashboardSqlDataSource1.ResultSchemaSerializable = resources.GetString("dashboardSqlDataSource1.ResultSchemaSerializable")
+            ' 
+            ' cardDashboardItem1
+            ' 
+            measure2.DataMember = "Extended Price"
+            measure2.SummaryType = DevExpress.DashboardCommon.SummaryType.StdDev
+            measure3.DataMember = "Extended Price"
+            measure3.SummaryType = DevExpress.DashboardCommon.SummaryType.StdDevp
+            cardCompactLayoutTemplate1.BottomSubValue1.DimensionIndex = 0
+            cardCompactLayoutTemplate1.BottomSubValue1.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.AbsoluteVariation
+            cardCompactLayoutTemplate1.BottomSubValue1.Visible = True
+            cardCompactLayoutTemplate1.BottomSubValue2.DimensionIndex = 0
+            cardCompactLayoutTemplate1.BottomSubValue2.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.PercentVariation
+            cardCompactLayoutTemplate1.BottomSubValue2.Visible = True
+            cardCompactLayoutTemplate1.BottomValue.DimensionIndex = 0
+            cardCompactLayoutTemplate1.BottomValue.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.ActualValue
+            cardCompactLayoutTemplate1.BottomValue.Visible = True
+            cardCompactLayoutTemplate1.DeltaIndicator.Visible = True
+            cardCompactLayoutTemplate1.MainValue.DimensionIndex = 0
+            cardCompactLayoutTemplate1.MainValue.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.Title
+            cardCompactLayoutTemplate1.MainValue.Visible = True
+            cardCompactLayoutTemplate1.MinWidth = 125
+            cardCompactLayoutTemplate1.Sparkline.Visible = True
+            cardCompactLayoutTemplate1.SubValue.DimensionIndex = 0
+            cardCompactLayoutTemplate1.SubValue.ValueType = DevExpress.DashboardCommon.CardRowDataElementType.Subtitle
+            cardCompactLayoutTemplate1.SubValue.Visible = True
+            card1.LayoutTemplate = cardCompactLayoutTemplate1
+            card1.AddDataItem("ActualValue", measure2)
+            card1.AddDataItem("TargetValue", measure3)
+            Me.cardDashboardItem1.Cards.AddRange(New DevExpress.DashboardCommon.Card() { card1})
+            Me.cardDashboardItem1.ComponentName = "cardDashboardItem1"
+            dimension3.DataMember = "Sales Person"
+            dimension4.DataMember = "OrderDate"
+            dimension4.DateTimeGroupInterval = DevExpress.DashboardCommon.DateTimeGroupInterval.MonthYear
+            Me.cardDashboardItem1.DataItemRepository.Clear()
+            Me.cardDashboardItem1.DataItemRepository.Add(measure2, "DataItem0")
+            Me.cardDashboardItem1.DataItemRepository.Add(measure3, "DataItem1")
+            Me.cardDashboardItem1.DataItemRepository.Add(dimension3, "DataItem2")
+            Me.cardDashboardItem1.DataItemRepository.Add(dimension4, "DataItem3")
+            Me.cardDashboardItem1.DataMember = "SalesPerson"
+            Me.cardDashboardItem1.DataSource = Me.dashboardSqlDataSource1
+            Me.cardDashboardItem1.InteractivityOptions.IgnoreMasterFilters = False
+            Me.cardDashboardItem1.Name = "Cards 1"
+            Me.cardDashboardItem1.SeriesDimensions.AddRange(New DevExpress.DashboardCommon.Dimension() { dimension3})
+            Me.cardDashboardItem1.ShowCaption = False
+            Me.cardDashboardItem1.SparklineArgument = dimension4
+            ' 
+            ' Dashboard1
+            ' 
+            Me.DataSources.AddRange(New DevExpress.DashboardCommon.IDashboardDataSource() { Me.dashboardSqlDataSource1})
+            Me.Items.AddRange(New DevExpress.DashboardCommon.DashboardItem() { Me.pieDashboardItem1, Me.cardDashboardItem1})
+            dashboardLayoutItem1.DashboardItem = Me.pieDashboardItem1
+            dashboardLayoutItem1.Weight = 34.41762854144806R
+            dashboardLayoutItem2.DashboardItem = Me.cardDashboardItem1
+            dashboardLayoutItem2.Weight = 65.582371458551947R
+            dashboardLayoutGroup1.ChildNodes.AddRange(New DevExpress.DashboardCommon.DashboardLayoutNode() { dashboardLayoutItem1, dashboardLayoutItem2})
+            dashboardLayoutGroup1.DashboardItem = Nothing
+            Me.LayoutRoot = dashboardLayoutGroup1
+            Me.Title.Text = "Dashboard"
+            CType(dimension1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(measure1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(dimension2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.pieDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.dashboardSqlDataSource1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(measure2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(measure3, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(dimension3, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(dimension4, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.cardDashboardItem1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+        End Sub
+
+        #End Region
+
+        Private dashboardSqlDataSource1 As DevExpress.DashboardCommon.DashboardSqlDataSource
+        Private pieDashboardItem1 As DevExpress.DashboardCommon.PieDashboardItem
+        Private cardDashboardItem1 As DevExpress.DashboardCommon.CardDashboardItem
+    End Class
+End Namespace
---
+++ VB/Dashboard_ClientDataCards_Wpf/MainWindow.xaml.vb
@@ -0,0 +1,87 @@
+﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon.ViewerData
+Imports DevExpress.Xpf.Charts
+Imports System
+Imports System.Data
+Imports System.Windows
+
+Namespace Dashboard_ClientDataCards_Wpf
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        ' Handles the DashboardControl_DashboardItemMouseUp event.
+        Private Sub DashboardControl_DashboardItemMouseUp(ByVal sender As Object, ByVal e As DevExpress.DashboardWpf.DashboardItemMouseActionWpfEventArgs)
+            If e.DashboardItemName = "cardDashboardItem1" And e.GetAxisPoint() IsNot Nothing Then
+                ' Obtains client data related to the clicked card.
+                Dim clickedItemData As MultiDimensionalData = e.GetSlice()
+                Dim delta As DeltaDescriptor = e.GetDeltas()(0)
+
+                ' Creates a data table that will be used to hold client data.
+                Dim dataSource As New DataTable()
+                dataSource.Columns.Add("Argument", GetType(Date))
+                dataSource.Columns.Add("Actual", GetType(Double))
+                dataSource.Columns.Add("Target", GetType(Double))
+
+                ' Saves values of axis points placed on the "sparkline" axis and corresponding
+                ' actual/target values to the data table.
+                For Each point As AxisPoint In clickedItemData.GetAxisPoints(DashboardDataAxisNames.SparklineAxis)
+                    Dim row As DataRow = dataSource.NewRow()
+                    Dim deltaValue As DeltaValue = clickedItemData.GetSlice(point).GetDeltaValue(delta)
+                    If deltaValue.ActualValue.Value IsNot Nothing AndAlso deltaValue.TargetValue.Value IsNot Nothing Then
+                        row("Argument") = point.Value
+                        row("Actual") = deltaValue.ActualValue.Value
+                        row("Target") = deltaValue.TargetValue.Value
+                    Else
+                        row("Argument") = DBNull.Value
+                        row("Actual") = DBNull.Value
+                        row("Target") = DBNull.Value
+                    End If
+                    dataSource.Rows.Add(row)
+                Next point
+                DisplayDetailedChart(GetFormTitle(clickedItemData), dataSource)
+            End If
+        End Sub
+
+        ' Creates a new form that is invoked on the card click and 
+        ' shows the chart displaying client data.
+        Private Sub DisplayDetailedChart(ByVal title As String, ByVal dataSource As DataTable)
+            Dim detailWindow As New DetailedChart()
+            detailWindow.Title = title
+
+            Dim chart As ChartControl = detailWindow.detailedChartControl
+            Dim diagram As New XYDiagram2D()
+            chart.Diagram = diagram
+            Dim series1 As New SplineAreaSeries2D()
+            series1.DisplayName = "Actual"
+            Dim series2 As New SplineAreaSeries2D()
+            series2.DisplayName = "Target"
+            diagram.Series.AddRange(New Series() { series1, series2 })
+
+            For Each series As Series In diagram.Series
+                series.DataSource = dataSource
+                series.ArgumentDataMember = "Argument"
+                series.ValueScaleType = ScaleType.Numerical
+            Next series
+
+            series1.ValueDataMember = "Actual"
+            series2.ValueDataMember = "Target"
+
+            detailWindow.ShowDialog()
+        End Sub
+
+        ' Obtains a value of the axis point placed on the "default" axis
+        ' to display this value in the invoked form title.
+        Private Function GetFormTitle(ByVal clickedItemData As MultiDimensionalData) As String
+            Dim clickedPoint As AxisPoint = clickedItemData.GetAxisPoints(DashboardDataAxisNames.DefaultAxis)(0)
+            Dim clickedPointValue As String = clickedPoint.Value.ToString()
+            Return clickedPointValue
+        End Function
+    End Class
+End Namespace
